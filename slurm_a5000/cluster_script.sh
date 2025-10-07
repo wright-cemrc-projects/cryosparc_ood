9730e8456c1b90b2023e624e661def032bdd1f4e
Matt Larson <mrlarson2@wisc.edu>
2025-10-07 13:41:33 -0500

    Defining r5000 vs a5000 queues.

---
+++ slurm_a5000/cluster_script.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name cryosparc_{{ project_uid }}_{{ job_uid }}
+#SBATCH --cpus-per-task={{ num_cpu }}
+#SBATCH --gres=gpu:{{ num_gpu }}
+#SBATCH --mem={{ ram_gb|int }}G
+#SBATCH --partition=a5000
+#SBATCH --comment="created by {{ cryosparc_username }}"
+#SBATCH --output={{ job_dir_abs }}/{{ project_uid }}_{{ job_uid }}_slurm.out
+#SBATCH --error={{ job_dir_abs }}/{{ project_uid }}_{{ job_uid }}_slurm.err
+
+{{ run_cmd }}
+
