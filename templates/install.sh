b61d2dd4348dbbc542a4ef75212bba54f2b93ebb
Matt Larson <mrlarson2@wisc.edu>
2025-10-28 12:20:27 -0500

    Joining commands for the queue.

--- templates/install.sh
+++ templates/install.sh
@@ -13,6 +13,9 @@ echo "Setting up in ${CHOME}"
 # Everything is relative to this script directory
 cd "$(dirname ${BASH_SOURCE[0]})"
 
+# Copy over the start-up splash pages
+cp -r pages ${CHOME}/pages
+
 # Copy over the queues, customize, and install
 mkdir -p ${CHOME}/queues
 
@@ -20,11 +23,13 @@ for SQ in "${SLURM_QUEUES[@]}"; do
   cp -r queues/slurm_template ${CHOME}/queues/${SQ}
   sed -i "s|{{CHOME}}|${CHOME}|g" ${CHOME}/queues/${SQ}/cluster_info.json 
   sed -i "s|{{QUEUE}}|${SQ}|g" ${CHOME}/queues/${SQ}/cluster_info.json 
-  # TODO: cd within that directory and call `cryosparm cluster connect`
 done
 
-# Copy over the start-up splash pages
-cp -r pages ${CHOME}/pages
+# Tell CryoSPARC about these configs; CryoSPARC must be running
+for SQ in "${SLURM_QUEUES[@]}"; do
+  cd ${CHOME}/queues/${SQ}
+  cryosparcm cluster connect
+done
 
 # DONE!
 echo "SLURM queue setup for CryoSPARC complete."
